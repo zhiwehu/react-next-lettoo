5f83fc491083ba1d5a0c94968073adfea48cdace
Jeffrey Hu <zhiwehu@gmail.com>
2022-03-28 17:35:52 +0800

    add projects

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,8 +8,56 @@ const projectsData = [
   {
     title: 'Text to Image',
     description: `Texttoimage is a Python library for converting text to a transparent image.`,
-    imgSrc: '/static/images/projects/text2image.jpg',
+    imgSrc: '/static/images/project/texttoimage.png',
     href: 'https://github.com/zhiwehu/texttoimage',
+  },
+  {
+    title: 'AI Code Player',
+    description: `This is a website for people learn English by using Mind Map. This website using the pure frontend technology and Markdown documents as the articles data files.`,
+    imgSrc: '/static/images/project/aicodeplayer.png',
+    href: 'https://aicodeplayer.com',
+  },
+  {
+    title: 'Zhixin Future',
+    description: `This is a online programming learning platform which developed by me. I used the React as the frontend and the Python/Django as the backend. This website integrated lots of technology such as online Python IDE, online C++ IDE and online Scratch IDE.`,
+    imgSrc: '/static/images/project/zhixinfuture.png',
+    href: 'https://zhixinfuture.com',
+  },
+  {
+    title: 'BookInaBox',
+    description: `Book In a Box is a company which provides Professional publishing services. I helped them to build the backend service.`,
+    imgSrc: '/static/images/project/bookinabox.png',
+    href: 'https://bookinabox.com',
+  },
+  {
+    title: 'Get Leverage',
+    description: `Leverage is the only dual-sided growth agency that optimizes revenue and performance by offering operational efficiency tactics and strategic marketing expertise to positively impact both top line revenue and bottom line expenditures. I helped them to build the backend core workflow platform which used lots of thirdparty sevices such as Slack、Trello, etc.`,
+    imgSrc: '/static/images/project/getleverage.png',
+    href: 'https://getleverage.com',
+  },
+  {
+    title: 'Less Doing',
+    description: `Less Doing, More Living. I helped this customer build the whole website and a iOS app named Calvin, it's a social group calendar app.`,
+    imgSrc: '/static/images/project/lessdoing.png',
+    href: '#',
+  },
+  {
+    title: 'Investormill',
+    description: `@Investormill  · Financial service.`,
+    imgSrc: '/static/images/project/invertormill.png',
+    href: 'https://www.facebook.com/Investormill/',
+  },
+  {
+    title: 'Calvinapp',
+    description: `Calvin app. Make plans happen.`,
+    imgSrc: '/static/images/project/calvinapp.png',
+    href: 'https://www.calvinapp.com',
+  },
+  {
+    title: 'Otipo',
+    description: `Otipo connects managers and staff to an interactive easy to use shift management solution`,
+    imgSrc: '/static/images/project/otipo.png',
+    href: 'https://otipo.com/',
   },
 ]
 
